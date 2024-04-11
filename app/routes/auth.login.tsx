95403b96a04513f3e24997e7f859834bf069dda7
Thai Pangsakulyanont <dtinth@spacet.me>
2024-04-11 16:49:50 +0000

    redirect to / after login

--- app/routes/auth.login.tsx
+++ app/routes/auth.login.tsx
@@ -1,9 +1,26 @@
 import { Container } from "@chakra-ui/react";
+import { useStore } from "@nanostores/react";
+import { useNavigate } from "@remix-run/react";
 import { Auth } from "@supabase/auth-ui-react";
 import { ThemeSupa } from "@supabase/auth-ui-shared";
+import { useEffect, useRef } from "react";
+import { backend } from "~/backend";
 import { singletonSupabase } from "~/backend/supabase";
 
 export default function AuthPage() {
+  const navigate = useNavigate();
+  const authState = useStore(backend.$authState);
+
+  const authenticated = authState.type === "authenticated";
+  const authenticatedRef = useRef(authenticated);
+
+  useEffect(() => {
+    if (!authenticatedRef.current && authenticated) {
+      navigate("/", { replace: true });
+    }
+    authenticatedRef.current = authenticated;
+  }, [authenticated, navigate]);
+
   return (
     <div>
       <Container maxW="md" height={16}>
