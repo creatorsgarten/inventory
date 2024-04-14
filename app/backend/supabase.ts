8d026666449aba9aeeec451f2a598118d8e13951
Thai Pangsakulyanont @ Surface Pro 7 WSL <dtinth@spacet.me>
2024-04-14 18:58:32 +0700

    add some data schema

--- app/backend/supabase.ts
+++ app/backend/supabase.ts
@@ -1,6 +1,7 @@
 import { createClient } from "@supabase/supabase-js";
+import { Database } from "./supabaseTypes";
 
-export const singletonSupabase = createClient(
+export const singletonSupabase = createClient<Database>(
   "http://localhost:54321",
   "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZS1kZW1vIiwicm9sZSI6ImFub24iLCJleHAiOjE5ODM4MTI5OTZ9.CRXP1A7WOeoJeXxjNni43kdQwgnWNReilDMblYTn_I0"
 );
