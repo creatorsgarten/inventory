6eeb252430d33ebd7e477a1998320068ce376028
Thai Pangsakulyanont @ Surface Pro 7 WSL <dtinth@spacet.me>
2024-04-14 18:38:37 +0700

    Fix problem where auth state is stuck in loading
    https://stackoverflow.com/questions/72385641/supabase-onauthstatechanged-how-do-i-properly-wait-for-the-request-to-finish-p

--- app/backend/InventoryBackendSupabase.ts
+++ app/backend/InventoryBackendSupabase.ts
@@ -1,3 +1,4 @@
+import { Session } from "@supabase/supabase-js";
 import { atom, onMount } from "nanostores";
 import { AuthState, InventoryBackend } from "./InventoryBackend";
 import { singletonSupabase } from "./supabase";
@@ -6,24 +7,33 @@ export class InventoryBackendSupabase implements InventoryBackend {
   $authState = atom<AuthState>({ type: "loading" });
   constructor(private supabase: typeof singletonSupabase) {
     onMount(this.$authState, () => {
+      const updateSession = (session: Session | null) => {
+        this.$authState.set(
+          session
+            ? {
+                type: "authenticated",
+                user: {
+                  uid: session.user.id,
+                  name:
+                    session.user.user_metadata.full_name ||
+                    session.user.email ||
+                    session.user.id,
+                },
+              }
+            : { type: "unauthenticated" }
+        );
+      };
       const {
         data: { subscription },
       } = this.supabase.auth.onAuthStateChange((event, session) => {
         if (event === "SIGNED_OUT") {
-          this.$authState.set({ type: "unauthenticated" });
+          updateSession(null);
         } else if (session) {
-          console.log("session", session);
-          this.$authState.set({
-            type: "authenticated",
-            user: {
-              uid: session.user.id,
-              name:
-                session.user.user_metadata.full_name ||
-                session.user.email ||
-                session.user.id,
-            },
-          });
+          updateSession(session);
         }
+      });
+      supabase.auth.getSession().then(({ data: { session } }) => {
+        updateSession(session);
       });
       return () => {
         subscription.unsubscribe();
