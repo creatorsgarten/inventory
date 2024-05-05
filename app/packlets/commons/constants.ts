5b5329bdb258fcc53eb98a40aed34223935d636a
Phumrapee Limpianchop <git@rayriffy.com>
2024-05-06 04:54:46 +0900

    feat: define common data type

---
+++ app/packlets/commons/constants.ts
@@ -0,0 +1,18 @@
+export enum TagType {
+  Item = 'item',
+  Container = 'container',
+}
+
+export enum ContainerType {
+  // box container will be allowed to contain items within it
+  Box = 'box',
+  // place container is a location, it cannot be put inside another container
+  Place = 'place',
+}
+
+export enum PossessionType {
+  // it's currently in user hand
+  User = 'user',
+  // it's currently in a container
+  Container = 'container',
+}
---
+++ app/packlets/commons/types.ts
@@ -0,0 +1,44 @@
+import {
+  ContainerType,
+  PossessionType,
+  TagType,
+} from '~/packlets/commons/constants'
+
+export interface Tag {
+  id: string
+  // if tag doesn't linked to anything yet, link is null
+  link: {
+    id: string
+    type: TagType
+  } | null
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface Item {
+  id: string
+  type: TagType.Item
+  possession: {
+    type: PossessionType
+    // if a possession type is user, then id is user id.
+    // otherwise, it's container id.
+    id: string
+  }
+  createdAt: Date
+  updatedAt: Date
+}
+
+export interface Container {
+  id: string
+  type: TagType.Container
+  containerType: ContainerType
+  // possession can be null if a container is a place
+  possession: {
+    type: PossessionType
+    // if a possession type is user, then id is user id.
+    // otherwise, it's container id.
+    id: string
+  } | null
+  createdAt: Date
+  updatedAt: Date
+}
