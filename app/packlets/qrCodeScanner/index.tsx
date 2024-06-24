19f97a8758e8b32f7296ae2f393bd22dafb56f4b
Phumrapee Limpianchop <git@rayriffy.com>
2024-06-24 18:02:40 +0900

    refactor: use lib scanner instead

--- app/packlets/qrCodeScanner/index.tsx
+++ app/packlets/qrCodeScanner/index.tsx
@@ -1,52 +1,32 @@
-import { VStack, chakra, Box, Text } from '@chakra-ui/react'
+import { chakra, Box, Text } from '@chakra-ui/react'
 import {
-  FunctionComponent, Suspense,
-  useRef,
+  FunctionComponent, PropsWithChildren, Suspense,
 } from 'react'
-
-import { useQRScanner } from './useQRScanner'
+import { QRScanner } from '@rayriffy/qr-scanner'
 
 interface Props {
   onScan?: (url: string) => void
 }
 
-const LoadedScanner: FunctionComponent<Props> = ({ onScan }) => {
-  const videoRef = useRef<HTMLVideoElement>(null)
-
-  const { initErrorMessage } = useQRScanner(videoRef, {
-    onResult: result => onScan?.(result.data),
-    onError: err => console.error(err),
-  })
-
-  if (initErrorMessage)
-    return (
-      <Box w="100%" border="1px" rounded="xl" py={16} borderStyle="dashed" borderColor="gray.500" textAlign="center">
-        <Text fontSize="md" fontWeight="bold">{initErrorMessage}</Text>
-      </Box>
-    )
-
+const Fallback: FunctionComponent<PropsWithChildren> = ({ children }) => {
   return (
-    <VStack align="start">
-      <chakra.video
-        ref={videoRef}
-        w="100%"
-        aspectRatio={1}
-        objectFit="cover"
-      />
-    </VStack>
+    <Box w="100%" border="1px" rounded="xl" py={16} borderStyle="dashed" borderColor="gray.500" textAlign="center">
+      <Text fontSize="md" fontWeight="bold">{children}</Text>
+    </Box>
   )
 }
 
-export const QRCodeScanner: FunctionComponent<Props> = props => {
-  const fallback = (
-    <Box w="100%" border="1px" rounded="xl" py={16} borderStyle="dashed" borderColor="gray.500" textAlign="center">
-      <Text fontSize="md" fontWeight="bold">Initializing QR code scanner</Text>
-    </Box>
-  )
+const CQRScanner = chakra(QRScanner)
 
+export const QRCodeScanner: FunctionComponent<Props> = ({ onScan }) => {
   return (
-    <Suspense fallback={fallback}>
-      <LoadedScanner {...props} />
+    <Suspense
+      fallback={<Fallback>Initializing QR code scanner</Fallback>}
+    >
+      <CQRScanner
+        error={message => <Fallback>{message}</Fallback>}
+        onScan={onScan}
+      />
     </Suspense>
   )
 }
--- app/packlets/qrCodeScanner/readerStateAtom.ts
+++
@@ -1,3 +0,0 @@
-import { atom } from 'nanostores'
-
-export const readerStateAtom = atom<'start' | 'stop'>('stop')
