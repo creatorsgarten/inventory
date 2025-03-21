a6bb1b1a14b4f43867057bc97f9f3edf489a3eb7
dtinth on MBP M1 <dtinth@spacet.me>
2025-03-22 02:49:24 +0700

    fix logic for getting backend mode

--- app/backend/backendMode.ts
+++ app/backend/backendMode.ts
@@ -6,10 +6,8 @@ if (typeof window !== "undefined") {
     window.location.hostname === "127.0.0.1" ||
     window.location.hostname === "::1"
   ) {
-    backendMode = "local";
+    backendMode = "prod";
   } else if (window.location.hostname === "mock.localhost") {
-    backendMode = "mock";
-  } else if (window.location.hostname === "legacy.localhost") {
     backendMode = "mock";
   } else {
     backendMode = "prod";
